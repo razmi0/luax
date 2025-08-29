20c889ee3bc298be31bdf66a5d3ef683b8680438
razmiO <thomas.cuesta31@gmail.com>
2025-08-29 17:57:41 +0200

    feat: rm pragma feat & stabilize target transpiled filename & add composability

--- lib/luax/transpiler/transpile.lua
+++ lib/luax/transpiler/transpile.lua
@@ -1,3 +1,4 @@
+local uv      = require("luv")
 local Fs      = require("lib.luax.utils.fs")
 local parse   = require("lib.luax.transpiler.parser_ast")
 local emit    = require("lib.luax.transpiler.code_gen")
@@ -8,13 +9,13 @@ local compose = require("lib.luax.transpiler.compose")
 ---@field SRC_PATH string
 ---@field BUILD_PATH string
 ---@field LUAX_FILE_EXTENSION string
----@field H_PRAGMA string
 ---@field RENDER_FUNCTION_NAME string
 ---@field RENDER_FUNCTION_PATH string
+---@field TARGET_FILE_EXTENSION string
 
 --> luax -> hyperscript --> index.html
 ---@param config TranspilerConfig
-local function transpile(uv, config)
+local function transpile(config)
     -- read one time instead of a lot ?
     local fs = Fs.new(uv)
     if not fs:has_subdir(config.SRC_PATH) then return end
@@ -25,24 +26,20 @@ local function transpile(uv, config)
             local path = config.SRC_PATH .. "/" .. file
             local content = fs:read(path)
             local ast = parse(content) -- parsing
-            local emitted = compose(
-                config.RENDER_FUNCTION_NAME,
-                config.RENDER_FUNCTION_PATH,
-                { -- store preambles/emitted and return it
-                    has_pragma = content:match(config.H_PRAGMA),
-                    has_map_helper = content:match(">.-{.-map%(.-%)"),
-                    has_filter_helper = content:match(">.-{.-filter%(.-%)")
-                },
-                function(accumulated)
+            local emitted = compose(content, config,
+                function(acc)
                     for _, node in ipairs(ast) do
-                        accumulated[#accumulated + 1] = emit(node, config.RENDER_FUNCTION_NAME)
+                        acc[#acc + 1] = emit(node, config.RENDER_FUNCTION_NAME)
                     end
+                    return acc
                 end)
-            --
-            local target_file_name = file:sub(1, #file - 1)
+            -- config.TARGET_FILE_EXTENSION
+            local target_file_name = file:gsub("%.[^.]*$", config.TARGET_FILE_EXTENSION)
             fs:write(config.BUILD_PATH .. "/" .. target_file_name, emitted)
         end
     end
+    uv.run()
 end
+
 
 return transpile
