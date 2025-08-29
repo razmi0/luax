85b07ad48a6d3e408de56da4b71b7858d99a1b17
razmiO <thomas.cuesta31@gmail.com>
2025-08-29 17:58:32 +0200

    feat: update with last changes

--- lib/luax/main.lua
+++ lib/luax/main.lua
@@ -1,26 +1,21 @@
 --TODO(1) : add metadata for preambles in parser ?
 --    (2) : luax embedded attributes in parser ?
 --    (3) : add a way to add custom preambles in parser ?
+--    (3) : explore /src/ recursively ?
 --
-local uv                   = require("luv")
-local to_ssg               = require("lib.luax.utils.to_ssg")
-local transpile            = require("lib.luax.transpiler.transpile")
+local to_ssg    = require("lib.luax.utils.to_ssg")
+local transpile = require("lib.luax.transpiler.transpile")
 --
-local RENDER_FUNCTION_NAME = "luax"
---
-
-transpile(uv, {
-    SRC_PATH             = "src",
-    BUILD_PATH           = "build",
-    LUAX_FILE_EXTENSION  = ".luax",
-    RENDER_FUNCTION_NAME = RENDER_FUNCTION_NAME,
-    RENDER_FUNCTION_PATH = "lib.luax.transpiler.luax",
-    H_PRAGMA             = "---@transpile " .. RENDER_FUNCTION_NAME,
+transpile({
+    SRC_PATH              = "src",                      -- path to luax files
+    BUILD_PATH            = "build",                    -- path to transpiled directory
+    LUAX_FILE_EXTENSION   = ".luax",                    -- extension use to detect luax files
+    TARGET_FILE_EXTENSION = ".lua",                     -- extension for transpiled files
+    RENDER_FUNCTION_NAME  = "Luax",                     -- the render function name
+    RENDER_FUNCTION_PATH  = "lib.luax.transpiler.luax", -- path to the render function
 })
 
 to_ssg({
     entry_path = "build.main",
     out_path = "index.html"
 })
-
-uv.run()
