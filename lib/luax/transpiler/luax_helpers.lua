bcf513f9802bf4d1b8934c0180f88a641b20132a
razmiO <thomas.cuesta31@gmail.com>
2025-09-02 14:34:54 +0200

    refactor: change luax helper inject logic to file parse

---
+++ lib/luax/transpiler/luax_helpers.lua
@@ -0,0 +1,35 @@
+local uv = require("luv")
+local fs = require("lib.luax.utils.fs").new(uv)
+local this_file_path = "/lib/luax/transpiler/luax_helpers.lua"
+
+local function map(tbl, func)
+    local newTbl = {}
+    for i, v in ipairs(tbl) do
+        table.insert(newTbl, func(v, i, tbl))
+    end
+    return table.concat(newTbl)
+end
+
+local function filter(tbl, func)
+    local newTbl = {}
+    for i, v in ipairs(tbl) do
+        if func(v, i, tbl) then
+            table.insert(newTbl, v)
+        end
+    end
+    return table.concat(newTbl)
+end
+
+local this_content = fs:read(uv.cwd() .. this_file_path)
+
+---@param fn_name "map"|"filter"
+local function get_helper(fn_name)
+    return this_content:match(
+        "(local%sfunction%s" ..
+        fn_name ..
+        ".-" ..
+        "return%stable%.concat%(newTbl%)%send%\n)"
+    )
+end
+
+return get_helper
