9e32ea0f3caa91d61b3eed92858ec54b070dfe49
razmiO <thomas.cuesta31@gmail.com>
2025-09-02 20:41:13 +0200

    refactor: add context object containing the transpiler information during run

--- lib/luax/transpiler/transpile.lua
+++ lib/luax/transpiler/transpile.lua
@@ -1,43 +1,48 @@
-local uv      = require("luv")
-local inspect = require("inspect")
-local parse   = require("lib.luax.transpiler.parser_ast")
-local emit    = require("lib.luax.transpiler.code_gen")
-local compose = require("lib.luax.transpiler.compose")
-local build   = require("lib.luax.transpiler.build")
-
----@class TranspilerConfig
----@field SRC_PATH string
----@field BUILD_PATH string
----@field LUAX_FILE_EXTENSION string
----@field RENDER_FUNCTION_NAME string
----@field RENDER_FUNCTION_PATH string
----@field TARGET_FILE_EXTENSION string
+local uv         = require("luv")
+local inspect    = require("inspect")
+local parse      = require("lib.luax.transpiler.parser_ast")
+local emitter    = require("lib.luax.transpiler.emitter")
+local emit       = require("lib.luax.transpiler.code_gen")
+local build_file = require("lib.luax.transpiler.build")
 
 
+--- Context is an object holding the transpiler information per file.
+--- parse and emitter method mutate it.
+---@class TranspilerContext
+---@field file File
+---@field config TranspilerConfig
+---@field ast LuaxAstNode[]
+---@field emitted string[]
 
+--- Luax transpiler main function
 ---@param config TranspilerConfig
-local function transpile(config)
-    build(
+local function transpile(config, plugins)
+    build_file(
         config,
         function(file)
+            --
             local ext = config.LUAX_FILE_EXTENSION
+            local target_ext = file.name:gsub("%.[^.]*$", config.TARGET_FILE_EXTENSION)
             if not file.name:sub(- #ext) == ext then return end
+
+            ---@type TranspilerContext
+            local ctx = {
+                file = file,
+                config = config,
+                emitted = {},
+                ast = {}
+            }
             --
-            local ast = parse(file.content, config)     -- parsing
-            local emitted = compose(                    -- code generation
-                file.content,
-                config,
-                function(acc)
-                    for _, node in ipairs(ast) do
-                        acc[#acc + 1] = emit(node, config)
-                    end
-                end)
+            parse(ctx)                  -- parsing (ctx.ast mutation)
+            emitter(ctx, function(_ctx) -- code generation (ctx.emitted mutation)
+                local emitted = _ctx.emitted
+                for _, node in ipairs(ctx.ast) do
+                    emitted[#emitted + 1] = emit(node, ctx.config)
+                end
+            end)
             --
-            return
-                file.name:gsub("%.[^.]*$", config.TARGET_FILE_EXTENSION),
-                emitted
-        end
-    )
+            return target_ext, ctx.emitted
+        end)
     uv.run()
 end
 
