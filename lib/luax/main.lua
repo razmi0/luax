b18efaa5057e38a68e26854c3dd59f7f2fb24eb7
razmiO <thomas.cuesta31@gmail.com>
2025-09-07 17:09:38 +0200

    doc: update todos

--- lib/luax/main.lua
+++ lib/luax/main.lua
@@ -1,7 +1,10 @@
 --    (2) : luax embedded attributes in parser ? (have to)
---    (3) : allow comments in luax
---    (4) : allow literal expressions in parser
+--    (3) : allow comments in luax ( do not strip them ? feature in config ?)
+--    (4) : allow literal array expressions in parser
 --    (5) : add line/col information if parser crash
+--    (6) : implement path alias ( done)
+--    (7) : no config first
+
 --
 local transpiler  = require("lib.luax.transpiler.transpile")
 local to_ssg      = require("lib.luax.utils.to_ssg")
