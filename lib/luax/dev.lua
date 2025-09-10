4d195bd93bceb6b23be2840e074b0270c08730bf
razmiO <thomas.cuesta31@gmail.com>
2025-09-10 18:42:56 +0200

    fix : watch ./build and bundle luax to _bundle.lua

--- lib/luax/dev.lua
+++ lib/luax/dev.lua
@@ -3,14 +3,13 @@ local uv              = require("luv") -- hyperfine : libUv > lib lfs
 local watcher         = require("watch")
 --
 
-local bundle_path     = "luajit lib/luax/bundle.lua"
+local bundle_path     = "luajit lib/luax/_bundle.lua"
 local not_bundle_path = "luajit lib/luax/main.lua"
 
 watcher.new({
     paths = { "./src" },
     recursive = true,
     exec = bundle_path,
-    -- ignore = { "index.html" },
 })
     :on("start")
     :on("change")
