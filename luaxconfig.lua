aa50a9697f2e8aaa702f3aa46a7376820fa58957
razmiO <thomas.cuesta31@gmail.com>
2025-09-17 01:02:10 +0200

    feat: minimal test conf

---
+++ luaxconfig.lua
@@ -0,0 +1,19 @@
+---@type PartialTranspilerConfig
+
+return {
+    base = "test",
+    root = "src",
+    headers = {
+        enabled = false
+    },
+    build = {
+        type = "module",
+        root_file = "main.lua",
+        out_file = "_app.lua",
+        out_dir = "build",
+    },
+    alias = {
+        ["@"] = "test/build"
+    }
+
+}
