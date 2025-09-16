2de7de60ab8e5a18838b09069709f6e36570c329
razmiO <thomas.cuesta31@gmail.com>
2025-09-17 01:06:39 +0200

    feat: remove pad start "/"

--- luax/utils/normalize_path.lua
+++ luax/utils/normalize_path.lua
@@ -1,6 +1,6 @@
 local function normalize_path(_p)
     local tmp_path = _p:match("^(.-)%.?l?u?a?$")
-    return tmp_path:gsub("%.", "/") .. ".lua"
+    return tmp_path:gsub("%.", "/"):gsub("^%/+", "")
 end
 
 return normalize_path
