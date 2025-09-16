77842f5a0cb659e1a951c4f0d3134e66ec9d3c8b
razmiO <thomas.cuesta31@gmail.com>
2025-09-17 01:05:56 +0200

    feat: add base & build.type props and integration

--- luax/transpiler/define_config.lua
+++ luax/transpiler/define_config.lua
@@ -1,103 +1,71 @@
 local deep_merge = require("luax.utils.deep_merge")
 local sort_aliases = require("luax.utils.sort_aliases")
+local normalize_path = require("luax.utils.normalize_path")
 local uv = require("luv")
 
+local function resolve_render_path(path)
+    local fd = uv.fs_open(path, "r", 438)
+    if fd then
+        uv.fs_close(fd)
+        return path
+    end
 
+    local filepath = package.searchpath(path, package.path)
+    if filepath then
+        return normalize_path(filepath)
+    end
 
+    error("render.lua not found. Tried user path '" .. path .. "' and library path.")
+end
 
 ---@param user_config PartialTranspilerConfig|nil
 ---@return TranspilerConfig
 local function define_config(user_config)
-    local flags, globals, rm_paths = (function()
-        local flag_map, globals, rm_paths = {}, {}, {}
-        local on_flag_args = function(start, str, callback)
-            local targets = {
-                global = function(s)
-                    return s:match("^%-%-globals?") or s:match("^%-%-G")
-                end,
-                rm_src = function(s)
-                    return s:match("^%-%-remove%-source") or s:match("^%-%-R")
-                end
-            }
-            for k, fn in pairs(targets) do
-                if fn(str) then
-                    for j = start + 1, #arg do
-                        if not arg[j] then return j end
-                        if arg[j]:match("^%-%-") then return j end
-                        callback(k, arg[j])
-                    end
-                end
-            end
-        end
-        for i = 1, #arg do
-            if arg[i]:match("^%-%-.-") then
-                flag_map[arg[i]] = true
-            end
-            on_flag_args(i, arg[i], function(type, _arg)
-                if type == "global" then
-                    globals[_arg] = true
-                elseif type == "rm_src" then
-                    rm_paths[#rm_paths + 1] = _arg
-                end
-            end)
-        end
-        return flag_map, globals, rm_paths
-    end)()
-
-    local function resolve_render_path(path)
-        local fd = uv.fs_open(path, "r", 438)
-        if fd then
-            uv.fs_close(fd)
-            return path
-        end
-
-        local filepath = package.searchpath(path, package.path)
-        if filepath then
-            return filepath
-        end
-
-        error("render.lua not found. Tried user path '" .. path .. "' and library path.")
-    end
-
-
-
     ---@type TranspilerConfig
     local defaults = {
+        base                 = "",
+        root                 = "src",
+        luax_file_extension  = ".luax",       --
+        render_function_name = "__lx__",      --
+        render_function_path = "luax/render", --
         headers              = {
             enabled            = true,
             transpiler_version = "0.0.1",
             licence            = "NO LICENCE",
             author             = "razmi0",
             repo_link          = "https://github.com/razmi0/luax",
-        },
-        cmd                  = {
-            flags = flags,
-            globals = globals,
-            rm_paths = rm_paths
-        },
-        luax_file_extension  = ".luax",
-        render_function_name = "__lx__",
-        render_function_path = "luax.render",
-        root                 = "src",
-        build                = {
+        }, --
+
+
+        build   = {
             bundle = true,
-            root = "build/main.lua",
-            out_file = "build/_app.lua",
+            root_file = "main.lua",
+            out_file = "_app.lua",
             out_dir = "build",
             no_emit = false,
             target_file_extension = ".lua",
-            empty_out_dir = true
+            empty_out_dir = true,
+            type = "none"
         },
-        plugins              = {},
-        alias                = {}
+        plugins = {},
+        alias   = {}
     }
 
     local cfg = deep_merge(defaults, user_config)
 
+
+    cfg.build.out_dir = cfg.base .. "/" .. cfg.build.out_dir
+    cfg.root = cfg.base .. "/" .. cfg.root
+    cfg.build.root_file = cfg.build.out_dir .. "/" .. cfg.build.root_file
+    cfg.build.out_file = cfg.build.out_dir .. "/" .. cfg.build.out_file
+
+
     -- aliases are sorted by specificity (longer aliases)
     -- means more specificity (prevent aliases replacing each other)
     cfg.alias = sort_aliases(cfg)
+
     cfg.render_function_path = resolve_render_path(cfg.render_function_path)
+    --
     return cfg
 end
 
